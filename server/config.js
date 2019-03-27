c712629b3dc849e33bebb010e3d5b95f4b8dfe61
Iwona Szerszeń <iwona.szerszen@outlook.com>
2019-03-27 17:53:00 +0100

    Update config.js by changing mongoURL - project connected to new data base on mLab servers

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
   //mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/wed-site',
-  mongoURL: process.env.MONGO_URL || 'mongodb://admin:admin1234@ds251284.mlab.com:51284/wed-site',
+  mongoURL: process.env.MONGO_URL || 'mongodb://admin:admin1234@ds125526.mlab.com:25526/wed-site-v1',
   port: process.env.PORT || 8000,
 };
 
